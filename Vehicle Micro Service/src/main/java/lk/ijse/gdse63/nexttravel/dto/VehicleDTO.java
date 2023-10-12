11c183cc0f4c2581d7c50818bb3356bc169c932a
Isuri001 <isuriupeksha258@gmail.com>
2023-10-12 22:18:07 +0530

    add vehicleDTO

--- Vehicle Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/dto/VehicleDTO.java
+++ Vehicle Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/dto/VehicleDTO.java
@@ -1,4 +1,26 @@
 package lk.ijse.gdse63.nexttravel.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VehicleDTO {
+    private int id;
+    private String name;
+    private String transmission;
+    private String category;
+    private String vehicleType;
+    private String seatCapacity;
+    private String fuelType;
+    private Double dayFee;
+    private Double fuelUsage;
+    private Double kmFee;
+    private Boolean hybrid;
+    private int driverId;
+    private String driverName;
+    private String contactNo;
+
 }
