dcc26918173e1b09ee76c4b210b3f3674bf9e4af
Isuri001 <isuriupeksha258@gmail.com>
2023-10-19 15:45:54 +0530

    updated

--- User Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/config/JwtAuthorizationFilter.java
+++ User Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/config/JwtAuthorizationFilter.java
@@ -6,7 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lk.ijse.gdse63.nexttravel.dto.AdminDTO;
+import lk.ijse.gdse63.nexttravel.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,22 +18,25 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
-    @Value("http://localhost:8080/api/v1/login")
+    @Value("${admin.data}")
     private String adminDataEndPoint;
 
     private final JwtUtil jwtUtil;
 
     private final ObjectMapper mapper;
+    private UserService service;
 
-    public JwtAuthorizationFilter(JwtUtil jwtUtil , ObjectMapper mapper ){
+    public JwtAuthorizationFilter(JwtUtil jwtUtil , ObjectMapper mapper , UserService service){
         this.jwtUtil = jwtUtil;
         this.mapper = mapper ;
+        this.service = service;
     }
 
 
@@ -51,26 +54,29 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             Claims claims = jwtUtil.resolveClaims(request);
 
             if (claims != null & jwtUtil.validateClaims(claims)){
+                Integer userId = (Integer) claims.get("userId");
                 String email = claims.getSubject();
-                String userName = (String) claims.get("userName");
-                String password = (String) claims.get("UserPassword");
                 ArrayList<String> o = (ArrayList<String>) claims.get("roles");
                 boolean admin = o.contains("User_Admin");
                 boolean u = o.contains("user");
-                if (admin || u ) {
-                    System.out.println(admin);
-                }
-                System.out.println("email : "+email);
-                System.out.println("User Name : "+userName);
-                System.out.println("Password : "+password);
 
-                //AdminDTO user = WebClient.create(adminDataEndPoint + "/" + email).get().retrieve().bodyToMono(AdminDTO.class).block();
-                //System.out.println(user);
-                Authentication authentication =
-                        new UsernamePasswordAuthenticationToken(email,"", new ArrayList<>());
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                if (u) {
+                    String[] split = request.getServletPath().split("/");
+                    String id = split[4];
+                    System.out.println(split.length-1);
+                    System.out.println(id);
+                    if (userId!=Integer.parseInt(id)){
+                        throw new AccessDeniedException("Access Denied");
+                    }
+                }
+                if (admin || u ) {
+                    Authentication authentication =
+                            new UsernamePasswordAuthenticationToken(email,"", new ArrayList<>());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
             }
         }catch (Exception e){
+            e.printStackTrace();
             errorDetails.put("message","Authentication Error");
             errorDetails.put("details",e.getMessage());
             response.setStatus(HttpStatus.FORBIDDEN.value());
