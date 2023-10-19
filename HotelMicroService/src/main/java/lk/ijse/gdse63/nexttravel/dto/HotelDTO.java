0b0986c39f706007c59211540ef21c3f7439429a
Isuri001 <isuriupeksha258@gmail.com>
2023-10-19 14:26:36 +0530

    updated HotelDTO

--- HotelMicroService/src/main/java/lk/ijse/gdse63/nexttravel/dto/HotelDTO.java
+++ HotelMicroService/src/main/java/lk/ijse/gdse63/nexttravel/dto/HotelDTO.java
@@ -4,17 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class HotelDTO {
     private int id;
     private String name;
-    private String address;
-    private String remark;
-    private String email;
     private String category;
-    private String contact;
+    private boolean petAllowed;
     private String maplink;
-    private String pets;
+    private String address;
+    private ArrayList<String> phone;
+    private String email;
+    private ArrayList<PricesDTO> prices;
+    private String remark;
+
 }
