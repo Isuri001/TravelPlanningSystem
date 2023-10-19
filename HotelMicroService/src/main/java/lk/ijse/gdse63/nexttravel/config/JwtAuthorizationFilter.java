ed1c62f983fc409a15cb2c0c73886abb197b2817
Isuri001 <isuriupeksha258@gmail.com>
2023-10-19 14:21:05 +0530

    updated JwtAuthorizationFilter in HotelmicroService

--- HotelMicroService/src/main/java/lk/ijse/gdse63/nexttravel/config/JwtAuthorizationFilter.java
+++ HotelMicroService/src/main/java/lk/ijse/gdse63/nexttravel/config/JwtAuthorizationFilter.java
@@ -6,8 +6,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lk.ijse.gdse63.nexttravel.dto.AdminDTO;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,7 +13,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,8 +21,6 @@ import java.util.Map;
 
 @Component
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
-    @Value("http://localhost:8080/api/v1/login")
-    private String adminDataEndPoint;
 
     private final JwtUtil jwtUtil;
 
@@ -51,32 +46,25 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             Claims claims = jwtUtil.resolveClaims(request);
 
             if (claims != null & jwtUtil.validateClaims(claims)){
+                Integer userId = (Integer) claims.get("userId");
                 String email = claims.getSubject();
-                String userName = (String) claims.get("userName");
-                String password = (String) claims.get("UserPassword");
                 ArrayList<String> o = (ArrayList<String>) claims.get("roles");
                 boolean admin = o.contains("Hotel_Admin");
+
                 if (admin) {
-                    System.out.println(admin);
+                    Authentication authentication =
+                            new UsernamePasswordAuthenticationToken(email,"", new ArrayList<>());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
-                System.out.println("email : "+email);
-                System.out.println("User Name : "+userName);
-                System.out.println("Password : "+password);
-                AdminDTO user = WebClient.create(adminDataEndPoint + "/" + email).get().retrieve().bodyToMono(AdminDTO.class).block();
-                System.out.println(user);
-                Authentication authentication =
-                        new UsernamePasswordAuthenticationToken(email,"", new ArrayList<>());
-                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }catch (Exception e){
+            e.printStackTrace();
             errorDetails.put("message","Authentication Error");
             errorDetails.put("details",e.getMessage());
             response.setStatus(HttpStatus.FORBIDDEN.value());
             response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-
             mapper.writeValue(response.getWriter(),errorDetails);
         }
-
         filterChain.doFilter(request,response);
     }
 }
