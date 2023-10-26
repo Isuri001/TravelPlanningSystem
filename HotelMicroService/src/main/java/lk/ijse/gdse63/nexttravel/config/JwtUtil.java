a0b5b090b5b523fe86e63a4883260be92bb0704f
Isuri001 <isuriupeksha258@gmail.com>
2023-10-26 19:27:04 +0530

    updated

--- HotelMicroService/src/main/java/lk/ijse/gdse63/nexttravel/config/JwtUtil.java
+++ HotelMicroService/src/main/java/lk/ijse/gdse63/nexttravel/config/JwtUtil.java
@@ -11,62 +11,63 @@ import javax.naming.AuthenticationException;
 import java.util.Date;
 import java.util.List;
 
+
 @Component
 public class JwtUtil {
-    private final String secret_key = "mesecretkey";
+    private final String secret_key = "mysecretkey";
 
     private final JwtParser jwtParser;
 
     private final String TOKEN_HEADER = "Authorization";
-
-    private final String TOKEN_PERFIX = "Bearer";
+    private final String TOKEN_PREFIX = "Bearer ";
 
     public JwtUtil(){
         this.jwtParser = Jwts.parser().setSigningKey(secret_key);
     }
 
-
-    private Claims parseJwtClaims(String token){
+    private Claims parseJwtClaims(String token) {
         return jwtParser.parseClaimsJws(token).getBody();
     }
 
-    public Claims resolveClaims(HttpServletRequest req){
+    public Claims resolveClaims(HttpServletRequest req) {
         try {
             String token = resolveToken(req);
-            if (token != null){
+            if (token != null) {
                 return parseJwtClaims(token);
             }
             return null;
-        }catch (ExpiredJwtException ex){
-            req.setAttribute("expired",ex.getMessage());
+        } catch (ExpiredJwtException ex) {
+            req.setAttribute("expired", ex.getMessage());
             throw ex;
-        }catch (Exception ex){
-            req.setAttribute("invalid",ex.getMessage());
+        } catch (Exception ex) {
+            req.setAttribute("invalid", ex.getMessage());
             throw ex;
         }
     }
 
     public String resolveToken(HttpServletRequest request) {
+
         String bearerToken = request.getHeader(TOKEN_HEADER);
-        if (bearerToken != null && bearerToken.startsWith(TOKEN_PERFIX)){
-            return bearerToken.substring(TOKEN_PERFIX.length());
+        if (bearerToken != null && bearerToken.startsWith(TOKEN_PREFIX)) {
+            return bearerToken.substring(TOKEN_PREFIX.length());
         }
         return null;
     }
 
-
-    public boolean validateClaims(Claims claims)throws AuthenticationException {
+    public boolean validateClaims(Claims claims) throws AuthenticationException {
         try {
             return claims.getExpiration().after(new Date());
-        }catch (Exception e){
+        } catch (Exception e) {
             throw e;
         }
     }
-    public String getEmail(Claims claims){
+
+    public String getEmail(Claims claims) {
         return claims.getSubject();
     }
 
-    private List<String> getRoles(Claims claims){
+    private List<String> getRoles(Claims claims) {
         return (List<String>) claims.get("roles");
     }
+
 }
