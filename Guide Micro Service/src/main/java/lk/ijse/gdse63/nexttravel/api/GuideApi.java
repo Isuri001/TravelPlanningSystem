542bfb0d1666474c482a58c1147f225994825d45
Isuri001 <isuriupeksha258@gmail.com>
2023-10-26 19:13:59 +0530

    updated

--- Guide Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/api/GuideApi.java
+++ Guide Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/api/GuideApi.java
@@ -5,25 +5,96 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
 @RestController
 @RequestMapping("/api/v1/guide")
 @CrossOrigin
 public class GuideApi {
-    @GetMapping(value = "/{id:\\d+}")
-    public ResponseEntity search(@PathVariable String id){
-        System.out.println("Search pressed :" +id );
-        return new ResponseEntity("Search Pressed"+ id , HttpStatus.OK);
-    }
+
     @PostMapping
-    public void save(@RequestBody GuideDTO guideDTO){
-        System.out.println("Save pressed :" + guideDTO );
+    public ResponseEntity saveGuide(@RequestParam("name") String name,
+                                    @RequestParam("address") String address,
+                                    @RequestParam("contact") String contact,
+                                    @RequestParam("birthdate")LocalDate birthDate,
+                                    @RequestParam("manDayValue") double manDayValue,
+                                    @RequestParam("experience") String experience,
+                                    @RequestPart("guideIdFront") byte[] guideIdFront,
+                                    @RequestPart("guideIdRear") byte[] guideIdRear,
+                                    @RequestPart("nicFront") byte[] nicFront,
+                                    @RequestPart("nicRear") byte[] nicRear,
+                                    @RequestPart("profilePic") byte[] profilePic) {
+
+        System.out.println("Name : "+name);
+        System.out.println("Address : "+address);
+        System.out.println("Contact : "+contact);
+        System.out.println("Birth Date : "+birthDate);
+        System.out.println("Man Day Value : "+manDayValue);
+        System.out.println("Experience : "+experience);
+        System.out.println("Guide Id Front : "+guideIdFront);
+        System.out.println("Guide Id Rear : "+guideIdRear);
+        System.out.println("NIC Front : "+nicFront);
+        System.out.println("NIC Rear : "+nicRear);
+        System.out.println("Profile Pic : "+profilePic);
+
+        return new ResponseEntity<>(1, HttpStatus.CREATED);
     }
-    @PutMapping
-    public void update(@RequestBody GuideDTO guideDTO){
-        System.out.println("Update pressed :" + guideDTO);
+
+    @DeleteMapping("/{id:\\d+}")
+    public ResponseEntity deleteGuide(@PathVariable int id) {
+        return ResponseEntity.ok(1);
     }
-    @DeleteMapping(value = "/{id:\\d+}")
-    public void delete(@PathVariable int id){
-        System.out.println("Delete pressed : " + id);
+
+    @PutMapping("/{id:\\d+}")
+    public ResponseEntity updateGuide(@PathVariable int id,
+                                      @RequestParam("name")String name,
+                                      @RequestParam("address")String address,
+                                      @RequestParam("contact") String contact,
+                                      @RequestParam("birthDate") LocalDate birthDate,
+                                      @RequestParam("manDayValue") double manDayValue,
+                                      @RequestParam("experience") String experience,
+                                      @RequestPart("guideIdFront") byte[] guideIdFront,
+                                      @RequestPart("guideIdRear") byte[] guideIdRear,
+                                      @RequestPart("nicFront") byte[] nicFront,
+                                      @RequestPart("nicRear") byte[] nicRear,
+                                      @RequestPart("profilePic") byte[] profilePic) {
+
+        GuideDTO guideDTO = new GuideDTO();
+        guideDTO.setId(id);
+        guideDTO.setName(name);
+        guideDTO.setAddress(address);
+        guideDTO.setContact(contact);
+        guideDTO.setBirthDate(birthDate);
+        guideDTO.setManDayValue(manDayValue);
+        guideDTO.setExperience(experience);
+        guideDTO.setGuideIdFront(guideIdFront);
+        guideDTO.setGuideIdRear(guideIdRear);
+        guideDTO.setNicFront(nicFront);
+        guideDTO.setNicRear(nicRear);
+        guideDTO.setProfilePic(profilePic);
+
+
+        return ResponseEntity.ok(guideDTO);
+
     }
+
+    @GetMapping("/{id:\\d+}")
+    public ResponseEntity getGuide(@PathVariable int id) {
+        GuideDTO guideDTO = new GuideDTO();
+        guideDTO.setId(id);
+        guideDTO.setName("Test");
+        guideDTO.setAddress("Test");
+        guideDTO.setContact("Test");
+        guideDTO.setBirthDate(LocalDate.now());
+        guideDTO.setManDayValue(1);
+        guideDTO.setExperience("Test");
+        guideDTO.setGuideIdFront(new byte[]{1,2,3});
+        guideDTO.setGuideIdRear(new byte[]{1,2,3});
+        guideDTO.setNicFront(new byte[]{1,2,3});
+        guideDTO.setNicRear(new byte[]{1,2,3});
+        guideDTO.setProfilePic(new byte[]{1,2,3});
+
+        return ResponseEntity.ok(guideDTO);
+    }
+
 }
