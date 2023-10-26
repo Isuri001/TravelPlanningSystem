88d5b3efebb49fb18ed1277a4fa04eb7d3f1d12c
Isuri001 <isuriupeksha258@gmail.com>
2023-10-27 00:00:20 +0530

    UserRepo updated

--- User Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/repo/UserRepo.java
+++ User Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/repo/UserRepo.java
@@ -8,22 +8,19 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.sql.Date;
 
-public interface UserRepo extends CrudRepository<User, Integer> {
 
+public interface UserRepo extends CrudRepository<User,Integer> {
     User findByEmail(String email);
-
     void deleteByEmail(String email);
-
     @Modifying
     @Transactional
-    @Query(value = "update user u set u.profile_pic = ? , u.nic_front_img = ? ," +
-    "u.nic_rear_img = ? where u.email = ?" ,
+    @Query(value = "update user u set u.profile_pic = ? , u.nic_front_img = ? , " +
+            "u.nic_rear_img = ? where u.email = ?",
             nativeQuery = true)
-
-    void updateImages(String profilePic, String nicFrontImg , String nicRearImg , String email);
+    void updateImages(String profilePic,String nicFrontImg,String nicRearImg, String email);
 
     @Modifying
-    @Query("UPDATE User u SET u.username = :username, u.password = :password, u.usernic = :usernic, u.contact = :contact,u.birthday = :birthday , u.gender = :gender , u.remarks = :remarks WHERE u.email = :email")
+    @Query("UPDATE User u SET u.username = :username, u.password = :password, u.usernic = :usernic, u.contact = :contact, u.birthday = :birthday, u.gender = :gender, u.remarks = :remarks WHERE u.email = :email")
     void updateUserInfoByEmail(
             String username,
             String password,
@@ -34,5 +31,4 @@ public interface UserRepo extends CrudRepository<User, Integer> {
             String gender,
             String remarks
     );
-
 }
