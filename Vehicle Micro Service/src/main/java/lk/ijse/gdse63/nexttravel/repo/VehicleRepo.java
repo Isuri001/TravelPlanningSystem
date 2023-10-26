34dec0b2b14e06dcb96feb9ae7ad09d61b9d92bd
Isuri001 <isuriupeksha258@gmail.com>
2023-10-27 00:20:45 +0530

    updated

--- Vehicle Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/repo/VehicleRepo.java
+++ Vehicle Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/repo/VehicleRepo.java
@@ -3,7 +3,8 @@ package lk.ijse.gdse63.nexttravel.repo;
 import lk.ijse.gdse63.nexttravel.entity.Vehicle;
 import org.springframework.data.repository.CrudRepository;
 
-public interface VehicleRepo extends CrudRepository<Vehicle, String> {
+import java.util.List;
 
-
+public interface VehicleRepo extends CrudRepository<Vehicle,Integer> {
+    List<Vehicle> findByCategory(String category);
 }
