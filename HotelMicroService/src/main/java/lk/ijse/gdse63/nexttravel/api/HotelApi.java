0cd0bb93830da926ef8adfeed53132406d05c426
Isuri001 <isuriupeksha258@gmail.com>
2023-10-26 19:25:06 +0530

    updated

--- HotelMicroService/src/main/java/lk/ijse/gdse63/nexttravel/api/HotelApi.java
+++ HotelMicroService/src/main/java/lk/ijse/gdse63/nexttravel/api/HotelApi.java
@@ -2,6 +2,7 @@ package lk.ijse.gdse63.nexttravel.api;
 
 import lk.ijse.gdse63.nexttravel.dto.HotelDTO;
 import lk.ijse.gdse63.nexttravel.dto.PricesDTO;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,59 +12,76 @@ import java.util.ArrayList;
 import java.util.stream.Stream;
 
 @RestController
-@RequestMapping("/api/v1/hotel")
-@CrossOrigin
+@RequestMapping("api/v1/hotel")
 public class HotelApi {
-    @GetMapping("{hotelId:\\d+}")
 
-    public void getHotel(@PathVariable int hotelId){
-        HotelDTO hotelDTO = new HotelDTO();
-        hotelDTO.setId(hotelId);
-        hotelDTO.setName("Hotel 1");
-        hotelDTO.setCategory("Hotel");
-        hotelDTO.setPetAllowed(true);
-        hotelDTO.setMaplink("www.google.com");
-        hotelDTO.setAddress("Colombo");
-        ArrayList<String> objects = new ArrayList<>();
-        Stream.of("0771234567","0771234587").forEach(objects::add);
-        hotelDTO.setPhone(objects);
-        hotelDTO.setEmail("M5Rq7@example.com");
-        hotelDTO.setPrices(new ArrayList<>());
-        hotelDTO.setRemark("Good");
-        System.out.println("Get Mapping");
+    private HotelService hotelService;
+
+    public HotelApi(HotelService hotelService){
+        this.hotelService = hotelService;
+    }
+
+    @GetMapping("{hotelId:\\d+}")
+    public ResponseEntity getHotel(@PathVariable int hotelId){
+        try {
+            HotelDTO search = hotelService.search(hotelId);
+            return ResponseEntity.ok(search);
+        } catch (NotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping()
-    public ResponseEntity uploadFiles(@RequestParam("files")MultipartFile[] files,
+    public ResponseEntity uploadFiles(@RequestParam("files") ArrayList<MultipartFile> files,
                                       @RequestParam("name") String name,
                                       @RequestParam("category") String category,
-                                      @RequestParam("petAllowed") Boolean petAllowed,
+                                      @RequestParam("petAllowed") boolean petAllowed,
                                       @RequestParam("mapLink") String mapLink,
                                       @RequestParam("address") String address,
+                                      @RequestParam("star") int star,
                                       @RequestParam("phone") ArrayList<String> phone,
                                       @RequestParam("email") String email,
                                       @RequestParam("prices") ArrayList<PricesDTO> prices,
-                                      @RequestParam("remarks") String remarks){
-        HotelDTO hotelDTO =  new HotelDTO();
+                                      @RequestParam("remarks") String remarks) {
+
+        ArrayList<byte[]> bytes = new ArrayList<>();
+        files.forEach(file -> {
+            try {
+                bytes.add(file.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        HotelDTO hotelDTO = new HotelDTO();
         hotelDTO.setName(name);
+        hotelDTO.setStar(star);
         hotelDTO.setCategory(category);
         hotelDTO.setPetAllowed(petAllowed);
-        hotelDTO.setMaplink(mapLink);
+        hotelDTO.setMapLink(mapLink);
         hotelDTO.setAddress(address);
         hotelDTO.setPhone(phone);
         hotelDTO.setEmail(email);
         hotelDTO.setPrices(prices);
-        hotelDTO.setRemark(remarks);
+        hotelDTO.setRemarks(remarks);
+        hotelDTO.setImages(bytes);
 
-        return new ResponseEntity(1,HttpStatus.CREATED);
+        try {
+            int save = hotelService.save(hotelDTO);
+            return new ResponseEntity(save, HttpStatus.CREATED);
+        } catch (SaveFailException e) {
+            e.printStackTrace();
+            return new ResponseEntity("Request Fail", HttpStatus.BAD_REQUEST);
+        }
+
 
     }
 
     @PutMapping("/{id:\\d+}")
-    public ResponseEntity update(@PathVariable String id,
-                                 @RequestParam("files") MultipartFile[] files,
+    public ResponseEntity update(@PathVariable int id,
+                                 @RequestParam("files") ArrayList<MultipartFile> files,
                                  @RequestParam("name") String name,
                                  @RequestParam("category") String category,
+                                 @RequestParam("star") int star,
                                  @RequestParam("petAllowed") boolean petAllowed,
                                  @RequestParam("mapLink") String mapLink,
                                  @RequestParam("address") String address,
@@ -71,28 +89,60 @@ public class HotelApi {
                                  @RequestParam("email") String email,
                                  @RequestParam("prices") ArrayList<PricesDTO> prices,
                                  @RequestParam("remarks") String remarks){
+        HotelDTO hotelDTO = new HotelDTO();
+        ArrayList<byte[]> bytes = new ArrayList<>();
+        files.forEach(file -> {
+            try {
+                bytes.add(file.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        hotelDTO.setId(id);
+        hotelDTO.setName(name);
+        hotelDTO.setCategory(category);
+        hotelDTO.setStar(star);
+        hotelDTO.setPetAllowed(petAllowed);
+        hotelDTO.setMapLink(mapLink);
+        hotelDTO.setAddress(address);
+        hotelDTO.setPhone(phone);
+        hotelDTO.setEmail(email);
+        hotelDTO.setPrices(prices);
+        hotelDTO.setRemarks(remarks);
+        hotelDTO.setImages(bytes);
+        try {
+            hotelService.update(hotelDTO);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (UpdateFailException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
 
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-
     @DeleteMapping("/{id:\\d+}")
-    public ResponseEntity delete(@PathVariable String id){
-        System.out.println(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity delete(@PathVariable int id){
+        try {
+            hotelService.delete(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (DeleteFailException | NotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/{starRate:^Star-[2-5]$}")
     public ResponseEntity getByStarRate(@PathVariable String starRate){
         int star = Integer.parseInt((starRate.split("-"))[1]);
-        System.out.println(star);
-        return new ResponseEntity<>(HttpStatus.OK);
+        try {
+            List<HotelDTO> byStarRate = hotelService.findByStarRate(star);
+            return new ResponseEntity<>(byStarRate, HttpStatus.OK);
+        } catch (ChangeSetPersister.NotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping
     public ResponseEntity getAll(){
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
 
 }
