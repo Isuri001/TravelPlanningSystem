47e109b07b683cbd70e228982a60f3081ef0126f
Isuri001 <isuriupeksha258@gmail.com>
2023-10-26 23:41:19 +0530

    updated

--- Travel Package Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/ServletInitializer.java
+++ Travel Package Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/ServletInitializer.java
@@ -3,6 +3,7 @@ package lk.ijse.gdse63.nexttravel;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
