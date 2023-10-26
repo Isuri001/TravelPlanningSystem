c12ace5f53cbb90204a5faa552c226adb14705e1
Isuri001 <isuriupeksha258@gmail.com>
2023-10-27 00:02:01 +0530

    updated

--- User Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/service/impl/UserServiceImpl.java
+++ User Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/service/impl/UserServiceImpl.java
@@ -30,23 +30,22 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 
+
 @Service
 public class UserServiceImpl implements UserService {
     @Value("${admin.data}")
-
     private String adminDataEndPoint;
+    private final UserRepo userRepo;
 
-    private final UserRepo userRepository;
-    private ModelMapper modelMapper;
-
-    public UserServiceImpl(UserRepo userRepository,ModelMapper mapper){
-        this.userRepository = userRepository;
-        this.modelMapper=mapper;
+    private final ModelMapper modelMapper;
+    public UserServiceImpl (UserRepo userRepo,ModelMapper mapper){
+        this.userRepo = userRepo;
+        this.modelMapper = mapper;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email);
+        User user = userRepo.findByEmail(email);
         System.out.println(user);
         List<String> roles = new ArrayList<>();
         roles.add("user");
@@ -59,14 +58,14 @@ public class UserServiceImpl implements UserService {
         return userDetails;
     }
 
-
     @Override
     public UserDTO searchUserByEmail(String email) throws UserNotFoundException {
         try {
-            User byEmail = userRepository.findByEmail(email);
+            User byEmail = userRepo.findByEmail(email);
             System.out.println(byEmail);
             UserDTO map = modelMapper.map(byEmail, UserDTO.class);
             importImages(byEmail,map);
+            //map.setNicImgs(jsonStringToArray(byEmail.getNicImgs()));
             return map;
         }catch (Exception e){
             e.printStackTrace();
@@ -76,14 +75,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void updateUser(UserDTO email) throws UpdateFailException {
-        try{
+        try {
             User user = modelMapper.map(email, User.class);
-            userRepository.updateUserInfoByEmail(user.getUsername(), user.getPassword(), user.getUsernic(), user.getContact(), user.getEmail(),
-                    user.getBirthday(), user.getGender(), user.getRemarks());
+            userRepo.updateUserInfoByEmail(user.getUsername(),user.getPassword(), user.getUsernic(), user.getContact(),
+                    user.getEmail(), user.getBirthday(), user.getGender(), user.getRemarks());
         }catch (Exception e){
-            throw new UpdateFailException("Operation Failed" ,e );
+            throw new UpdateFailException("Operation Failed",e);
         }
-
     }
 
     @Override
@@ -91,23 +89,22 @@ public class UserServiceImpl implements UserService {
     public int addUsers(UserDTO userDTO) throws CreateFailException {
         try {
             User user = modelMapper.map(userDTO, User.class);
-            User save = userRepository.save(user);
+            User save = userRepo.save(user);
             exportImages(userDTO,user);
-            userRepository.updateImages(save.getProfilePic(), save.getNicFrontImg(), save.getNicRearImg(), save.getEmail());
+            userRepo.updateImages(save.getProfilePic(),save.getNicFrontImg(),save.getNicRearImg(),save.getEmail());
             return save.getId();
-        }catch (Exception e ){
-            throw new CreateFailException("Operation Failed" ,e );
+        }catch (Exception e){
+            throw new CreateFailException("Operation Failed",e);
         }
     }
 
     @Override
     public void deleteUser(String email) throws DeleteFailException {
         try {
-            userRepository.deleteByEmail(email);
+            userRepo.deleteByEmail(email);
         }catch (Exception e){
-            throw new DeleteFailException("Operation Failed" ,e );
+            throw new DeleteFailException("Operation Failed",e);
         }
-
     }
 
     @Override
@@ -116,61 +113,56 @@ public class UserServiceImpl implements UserService {
     }
 
     ArrayList<String> jsonStringToArray(String jsonString) throws JSONException {
-        ArrayList<String> stringArray = new ArrayList<>();
-
-
+        ArrayList<String> stringArray = new ArrayList<String>();
         JSONArray jsonArray = new JSONArray(jsonString);
-
-        for (int i =0; i<jsonArray.length();i++){
+        for (int i = 0; i < jsonArray.length(); i++) {
             stringArray.add(jsonArray.getString(i));
         }
         return stringArray;
     }
 
-    public void exportImages(UserDTO userDTO, User user) throws Exception{
-        String dt = LocalDate.now().toString().replace("-" , "_" ) + "__"
-                + LocalTime.now().toString().replace(":" , "_");
+    public void exportImages(UserDTO userDTO,User user) throws IOException {
+        String dt = LocalDate.now().toString().replace("-", "_") + "__"
+                + LocalTime.now().toString().replace(":", "_");
 
         InputStream is = new ByteArrayInputStream(userDTO.getProfilePicByte());
         BufferedImage bi = ImageIO.read(is);
-        File outputFile = new File("images/pto_pic/"+dt+ ".jpg");
-        ImageIO.write(bi, "jpg", outputFile);
-        user.setProfilePic(outputFile.getAbsolutePath());
+        File outputfile = new File("images/user/pro_pic/"+dt+ ".jpg");
+        ImageIO.write(bi, "jpg", outputfile);
+        user.setProfilePic(outputfile.getAbsolutePath());
 
         InputStream is1 = new ByteArrayInputStream(userDTO.getNicFrontByte());
         BufferedImage bi1 = ImageIO.read(is1);
-        File outputFile1 = new File("images/nic_front/"+dt+ ".jpg");
-        ImageIO.write(bi1, "jpg", outputFile1);
-        user.setNicFrontImg(outputFile1.getAbsolutePath());
+        File outputfile1 = new File("images/user/nic_front/"+dt+ ".jpg");
+        ImageIO.write(bi1, "jpg", outputfile1);
+        user.setNicFrontImg(outputfile1.getAbsolutePath());
 
         InputStream is2 = new ByteArrayInputStream(userDTO.getNicRearByte());
         BufferedImage bi2 = ImageIO.read(is2);
-        File outputFile2 = new File("images/nic_rear/"+dt+ ".jpg");
-        ImageIO.write(bi2, "jpg", outputFile2);
-        user.setNicRearImg(outputFile2.getAbsolutePath());
-
+        File outputfile2 = new File("images/user/nic_rear/"+dt+ ".jpg");
+        ImageIO.write(bi2, "jpg", outputfile2);
+        user.setNicRearImg(outputfile2.getAbsolutePath());
     }
 
-    public void importImages(User user, UserDTO userDTO) throws IOException {
 
+    public void importImages(User user,UserDTO userDTO) throws IOException {
         BufferedImage read = ImageIO.read(new File(user.getProfilePic()));
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(read, "jpg" , baos);
+        ImageIO.write(read, "jpg", baos);
         byte[] bytes = baos.toByteArray();
         userDTO.setProfilePic(Base64.getEncoder().encodeToString(bytes));
 
         BufferedImage read1 = ImageIO.read(new File(user.getNicFrontImg()));
         ByteArrayOutputStream baos1 = new ByteArrayOutputStream();
-        ImageIO.write(read1, "jpg" , baos1);
+        ImageIO.write(read1, "jpg", baos1);
         byte[] bytes1 = baos.toByteArray();
         userDTO.setProfilePic(Base64.getEncoder().encodeToString(bytes1));
 
         BufferedImage read2 = ImageIO.read(new File(user.getNicRearImg()));
         ByteArrayOutputStream baos2 = new ByteArrayOutputStream();
-        ImageIO.write(read2, "jpg" , baos2);
-        byte[] bytes2 = baos2.toByteArray();
+        ImageIO.write(read2, "jpg", baos2);
+        byte[] bytes2 = baos.toByteArray();
         userDTO.setProfilePic(Base64.getEncoder().encodeToString(bytes2));
-
     }
 
 }
