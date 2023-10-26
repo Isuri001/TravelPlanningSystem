96b49d64d5f967be4a367ba6925ffec514eb9d65
Isuri001 <isuriupeksha258@gmail.com>
2023-10-26 21:15:09 +0530

    updated

--- Travel Package Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/service/TravelPackageService.java
+++ Travel Package Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/service/TravelPackageService.java
@@ -1,6 +1,21 @@
 package lk.ijse.gdse63.nexttravel.service;
 
 
+import lk.ijse.gdse63.nexttravel.dto.TravelPackageDTO;
+import lk.ijse.gdse63.nexttravel.exception.DeleteFailException;
+import lk.ijse.gdse63.nexttravel.exception.NotFoundException;
+import lk.ijse.gdse63.nexttravel.exception.SaveFailException;
+import lk.ijse.gdse63.nexttravel.exception.UpdateFailException;
+
+import java.util.List;
+
 public interface TravelPackageService {
 
+    public String save(TravelPackageDTO obj) throws SaveFailException;
+    void update(TravelPackageDTO obj) throws UpdateFailException;
+    void delete(String id) throws DeleteFailException;
+    List<TravelPackageDTO> getPackagesByCategory(String category);
+    TravelPackageDTO fidById(String id) throws NotFoundException;
+    List<TravelPackageDTO> findByCategory(String category) throws NotFoundException;
+    String generateNextId();
 }
