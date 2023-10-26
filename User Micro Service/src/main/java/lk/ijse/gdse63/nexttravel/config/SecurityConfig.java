1088cd88338ca192563bfd119828800e5bf50d8a
Isuri001 <isuriupeksha258@gmail.com>
2023-10-26 23:53:29 +0530

    updated

--- User Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/config/SecurityConfig.java
+++ User Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/config/SecurityConfig.java
@@ -9,32 +9,37 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @Configuration
 @EnableWebSecurity
+@CrossOrigin
 public class SecurityConfig {
     @Autowired
     private JwtAuthorizationFilter jwtAuthorizationFilter;
-
     private final UserService userService;
 
-    public SecurityConfig(UserService userService){
+    public SecurityConfig(UserService userService) {
         this.userService = userService;
+
     }
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http , NoOpPasswordEncoder noOpPasswordEncoder)throws Exception{
+    public AuthenticationManager authenticationManager(HttpSecurity http, NoOpPasswordEncoder noOpPasswordEncoder)
+            throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
         authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(noOpPasswordEncoder);
         return authenticationManagerBuilder.build();
     }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .requestMatchers(HttpMethod.POST,"/api/v1/user/login").permitAll()
@@ -43,11 +48,18 @@ public class SecurityConfig {
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
+        CorsConfigurer<HttpSecurity> cors = http.cors();
+        cors.and().authorizeRequests();
+
         return http.build();
     }
 
+    
+    @SuppressWarnings("deprecation")
     @Bean
-    public NoOpPasswordEncoder passwordEncoder(){
+    public NoOpPasswordEncoder passwordEncoder() {
         return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
+
+
 }
