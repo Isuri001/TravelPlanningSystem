dc388c68373eb96c79b7f71c84bea1825600581f
Isuri001 <isuriupeksha258@gmail.com>
2023-10-27 00:30:48 +0530

    updated

--- Vehicle Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/service/impl/VehicleServiceImpl.java
+++ Vehicle Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/service/impl/VehicleServiceImpl.java
@@ -1,6 +1,10 @@
 package lk.ijse.gdse63.nexttravel.service.impl;
 
 
+import lk.ijse.gdse63.nexttravel.Exception.SaveFailException;
+import lk.ijse.gdse63.nexttravel.dto.VehicleDTO;
+import lk.ijse.gdse63.nexttravel.entity.Driver;
+import lk.ijse.gdse63.nexttravel.entity.Vehicle;
 import lk.ijse.gdse63.nexttravel.repo.DriverRepo;
 import lk.ijse.gdse63.nexttravel.repo.VehicleRepo;
 import lk.ijse.gdse63.nexttravel.service.VehicleService;
@@ -20,10 +24,25 @@ public class VehicleServiceIMPL implements VehicleService {
     Gson gson;
     VehicleRepo vehicleRepo;
     public VehicleServiceIMPL(DriverRepo driverRepo, VehicleRepo vehicleRepo,
-                              ModelMapper modelMapper, Gson gsonr) {
+                              ModelMapper modelMapper, Gson gson) {
         this.driverRepo = driverRepo;
         this.vehicleRepo = vehicleRepo;
         this.modelMapper = modelMapper;
         this.gson = gsonr;
+
+    }
+
+    @Override
+    public int saveVehicle(VehicleDTO dto) throws SaveFailException {
+        try {
+            Vehicle vehicle = modelMapper.map(dto, Vehicle.class);
+            Driver driver = modelMapper.map(dto.getDriverDTO(), Driver.class);
+            exportImages(dto,driver,vehicle);
+            Driver save = driverRepo.save(driver);
+            vehicle.setDriver(save);
+            return vehicleRepo.save(vehicle).getId();
+        }catch (Exception e){
+            throw new SaveFailException("Save Fail ",e);
+        }
 
     }
