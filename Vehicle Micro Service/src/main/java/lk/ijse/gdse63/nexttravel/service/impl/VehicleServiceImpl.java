631ba3ddbbd824f19f6b98edbecd9b88df263233
Isuri001 <isuriupeksha258@gmail.com>
2023-10-27 00:25:38 +0530

    updated

--- Vehicle Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/service/impl/VehicleServiceImpl.java
+++ Vehicle Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/service/impl/VehicleServiceImpl.java
@@ -1,8 +1,10 @@
 package lk.ijse.gdse63.nexttravel.service.impl;
 
 
+import lk.ijse.gdse63.nexttravel.repo.DriverRepo;
 import lk.ijse.gdse63.nexttravel.repo.VehicleRepo;
 import lk.ijse.gdse63.nexttravel.service.VehicleService;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -10,14 +12,18 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Service
-public class VehicleServiceImpl implements VehicleService {
+public class VehicleServiceIMPL implements VehicleService {
+    DriverRepo driverRepo;
+    ModelMapper modelMapper;
+    Gson gson;
+    VehicleRepo vehicleRepo;
+    public VehicleServiceIMPL(DriverRepo driverRepo, VehicleRepo vehicleRepo,
+                              ModelMapper modelMapper, Gson gsonr) {
+        this.driverRepo = driverRepo;
+        this.vehicleRepo = vehicleRepo;
+        this.modelMapper = modelMapper;
+        this.gson = gsonr;
 
-    private final VehicleRepo vehicleRepository;
-
-    public VehicleServiceImpl(VehicleRepo vehicleRepository){
-        this.vehicleRepository = vehicleRepository;
     }
-
-
-}
