2793cf7b7b8c28b3a4ce0dc97bc7e419f831e2ee
Isuri001 <isuriupeksha258@gmail.com>
2023-10-26 21:13:45 +0530

    add TravelPackageRepo

--- Travel Package Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/repo/TravelPackageRepo.java
+++ Travel Package Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/repo/TravelPackageRepo.java
@@ -4,7 +4,10 @@ package lk.ijse.gdse63.nexttravel.repo;
 import lk.ijse.gdse63.nexttravel.entity.TravelPackage;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TravelPackageRepo extends CrudRepository<TravelPackage, String> {
+import java.util.List;
 
+public interface TravelPackageRepo extends CrudRepository<TravelPackage, String> {
+    List<TravelPackage> findByCategory(String category);
+    List<TravelPackage> findAll();
 
 }
