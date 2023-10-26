cffe8240cd4864752f47d48df4fbb4579ab86af8
Isuri001 <isuriupeksha258@gmail.com>
2023-10-27 00:22:38 +0530

    add changes

--- Vehicle Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/service/VehicleService.java
+++ Vehicle Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/service/VehicleService.java
@@ -1,6 +1,22 @@
 package lk.ijse.gdse63.nexttravel.service;
 
+import lk.ijse.gdse63.nexttravel.Exception.DeleteFailException;
+import lk.ijse.gdse63.nexttravel.Exception.NotFoundException;
+import lk.ijse.gdse63.nexttravel.Exception.SaveFailException;
+import lk.ijse.gdse63.nexttravel.Exception.UpdatefailException;
+import lk.ijse.gdse63.nexttravel.dto.VehicleDTO;
 
-public interface VehicleService{
+import java.util.List;
+
+public interface VehicleService {
+    int saveVehicle(VehicleDTO dto) throws SaveFailException;
+
+    VehicleDTO searchVehicle(int id) throws NotFoundException;
+
+    List<VehicleDTO> searchByCategory(String category) throws NotFoundException;
+
+    void updateVehicle(VehicleDTO dto) throws UpdatefailException;
+
+    void deleteVehicle(int id) throws NotFoundException, DeleteFailException;
 
 }
