317597db4a4d70b5d0237872d06d5316b8e22e27
Isuri001 <isuriupeksha258@gmail.com>
2023-10-26 19:19:13 +0530

    change

--- Guide Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/repo/GuideRepo.java
+++
@@ -1,9 +0,0 @@
-package lk.ijse.gdse63.nexttravel.repo;
-
-import lk.ijse.gdse63.nexttravel.entity.Guide;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GuideRepo extends CrudRepository<Guide, String> {
-
-
-}
