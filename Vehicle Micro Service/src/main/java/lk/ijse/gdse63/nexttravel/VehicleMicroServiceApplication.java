97782d4413d58fdfb939fcfa51c022db7dc72339
Isuri001 <isuriupeksha258@gmail.com>
2023-10-27 00:45:42 +0530

    updated

--- Vehicle Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/VehicleMicroServiceApplication.java
+++ Vehicle Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/VehicleMicroServiceApplication.java
@@ -1,15 +1,16 @@
 package lk.ijse.gdse63.nexttravel;
 
+import com.google.gson.Gson;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class VehicalMicroServiceApplication {
+public class VehicleMicroServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(VehicalMicroServiceApplication.class, args);
+        SpringApplication.run(VehicleMicroServiceApplication.class, args);
     }
 
     @Bean
