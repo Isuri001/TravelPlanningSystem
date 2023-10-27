f88b510d6efb05282f598ed46f51b730ddd6f878
Isuri001 <isuriupeksha258@gmail.com>
2023-10-27 12:34:42 +0530

    update repo

---
+++ Vehicle Micro Service/src/main/java/lk/ijse/gdse63/nexttravel/repo/DriverRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse63.nexttravel.repo;
+
+import lk.ijse.gdse63.nexttravel.entity.Driver;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DriverRepo extends CrudRepository<Driver,Integer> {
+}
