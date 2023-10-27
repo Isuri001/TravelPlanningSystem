8204ab5bfe368ae5ea90bdd808c7bf0ebe64ece2
Isuri001 <isuriupeksha258@gmail.com>
2023-10-27 13:58:14 +0530

    new updates

---
+++ UserTravelMicroService/src/test/java/lk/ijse/gdse63/nexttravel/UserTravelMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse63.nexttravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTravelMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
